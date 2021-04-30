bb0d467af5d4c3c02de4faf44d65a1e3d8e03f30
mattiadebonis <mattia.debonis@gmail.com>
2021-04-30 16:19:18 +0200

    Formattazione prima lettera cognome maiuscola

--- js/script.js
+++ js/script.js
@@ -1,17 +1,41 @@
 // 1. chiedi all'utente il cognome
 
 var cognome = prompt("Inserisci il tuo cognome");
+var cognomeCorretto = ""
 var array = ["Bianchi", "Verdi", "Rossi", "Neri", "Gialli"];
 // 2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
 
 console.log(array);
 
-array.push(cognome);
+for (i=0; i<cognome.length; i++){
+    console.log(cognome[i])
+    if (i == 0){
+        cognomeCorretto = cognome[i].toUpperCase();
+    }else{
+        cognomeCorretto = cognomeCorretto + cognome[i];  
+    }
+}
 
-console.log(array);
+array.push(cognomeCorretto);
 
 // 3. stampa la lista ordinata alfabeticamente
 
 array.sort();
 console.log(array);
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+
+
+// for (i=0; i<cognome.length; i++){
+//     console.log(cognome[i])
+//     if (i == 0){
+//         if (cognome[i] == cognome[i].toUpperCase){
+//             cognome = cognome + cognome[i];
+//         }else{
+//             cognome = cognome + cognome[i].toUppercase;
+//         }
+//     }else{
+//         cognome = cognome + cognome[i].toLowercase;
+//     }
+
+// }
