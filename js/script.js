72cd14a9bfbe21f8adc06bf71b9f1beb41783662
mattiadebonis <mattia.debonis@gmail.com>
2021-04-30 16:24:07 +0200

    Calcolo posizione umana

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@
 var cognome = prompt("Inserisci il tuo cognome");
 var cognomeCorretto = ""
 var array = ["Bianchi", "Verdi", "Rossi", "Neri", "Gialli"];
+var posizioneUmana = 0;
 // 2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
 
 console.log(array);
 
-for (i=0; i<cognome.length; i++){
+for (var i=0; i<cognome.length; i++){
     console.log(cognome[i])
     if (i == 0){
         cognomeCorretto = cognome[i].toUpperCase();
@@ -24,18 +25,11 @@ array.sort();
 console.log(array);
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 
+for (var a=0; a<array.length; a++){
+    if (array[a] == cognomeCorretto){
+        posizioneUmana = a + 1;
+        console.log("sei in posizione" + posizioneUmana);
 
+    }
 
-// for (i=0; i<cognome.length; i++){
-//     console.log(cognome[i])
-//     if (i == 0){
-//         if (cognome[i] == cognome[i].toUpperCase){
-//             cognome = cognome + cognome[i];
-//         }else{
-//             cognome = cognome + cognome[i].toUppercase;
-//         }
-//     }else{
-//         cognome = cognome + cognome[i].toLowercase;
-//     }
-
-// }
+}
